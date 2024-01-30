19bbc5dc0cb3d97792d1c07e5a79a4f47b1a0c4c
nam32 <tjanjumratsang@gmail.com>
2024-01-30 21:11:41 +0100

    Update 6_convert_GRCh38_fa_to_2bit.sh

--- 6_convert_GRCh38_fa_to_2bit.sh
+++ 6_convert_GRCh38_fa_to_2bit.sh
@@ -4,14 +4,12 @@
 #SBATCH --mem-per-cpu=25000MB
 #SBATCH --job-name=preprocessing_target_sequence
 #SBATCH --time=02:00:00
-#SBATCH --mail-user=titaporn.janjumratsang@students.unibe.ch
-#SBATCH --mail-type=begin,end
 
 ####
 
-# input: Homo_sapiens.GRCh38.108.gtf, *_GRCh38_sorted.bam
-# output: biotype_counts_processed.txt, biotype_counts_rawfile.txt
-# This script generate count table
+# input: Homo_sapiens.GRCh38.dna.primary_assembly.fa
+# output: Homo_sapiens.GRCh38.dna.primary_assembly.2bit
+# This script convert FASTA file into TwoBit format
 
 ####
 
